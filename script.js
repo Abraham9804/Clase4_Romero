ea670982c60d6f7017415bc6fec1bb0b49e9e792
Romero <romero9804@hotmail.com>
2022-06-19 12:23:14 -0700

    validacion 3 meses

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ while((isNaN(cantidad) || isNaN(meses)) || cantidad <= 1000 || cantidad >50000 |
     cantidad = parseFloat(prompt("ingrese la cantidad deseada"))
     meses = parseInt(prompt("Ingrese los meses a pagar"))
 
-    if(meses <= 3 ){
+    if(meses <= 3 && cantidad >= 1000 || cantidad <=50000){
         interes = 0.10
         total = cantidad + (cantidad * interes)
         pagos = total / meses
         console.log("el monto total a pagar es: " + total + " pesos y los pagos a " + meses + " meses son de: " + pagos.toFixed(2) + " pesos")
     }
 
-    else if(meses >3 && meses <= 6) {
+    else if(meses >3 && meses <= 6 && cantidad >= 1000 || cantidad <=50000) {
         interes = 0.25
         total = cantidad + (cantidad * interes)
         pagos = total / meses
@@ -24,7 +24,7 @@ while((isNaN(cantidad) || isNaN(meses)) || cantidad <= 1000 || cantidad >50000 |
     
     }
 
-    else if(meses >6 && meses <= 12) {
+    else if(meses >6 && meses <= 12 && cantidad >= 1000 || cantidad <=50000) {
         interes = 0.50
         total = cantidad + (cantidad * interes)
         pagos = total / meses
